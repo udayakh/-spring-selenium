d7ce1d53d7a9bd712bdd9f9fc2f880c08bfb6eee
udayakh <udaykh4@gmail.com>
2022-10-15 11:24:43 +0100

    commit

---
+++ src/test/java/com/selenium/SpringSeleniumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.selenium;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSeleniumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
